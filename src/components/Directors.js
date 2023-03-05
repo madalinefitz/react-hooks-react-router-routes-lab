e591a503c498ebc3b90058df9e06626f895dabae
madalinefitzpatrick <madaline.fitzpatrick@gmail.com>
2023-03-05 18:05:21 -0500

    deliverables met

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,28 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  
+const directorsComponent = directors.map(director => {
+  return (
+    <div>
+      <h2>{director.name}</h2>
+      <ul> Movies:
+        {(director.movies).map(movie => {
+          return (
+            <li>{movie}</li>
+          )
+        })}
+      </ul>
+    </div>
+  )
+})
+  
+  return (
+    <div>
+      <h1>Directors Page</h1>
+      {directorsComponent}
+    </div>
+  )
 }
 
 export default Directors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,30 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+
+ 
+
+  const movieComponents = movies.map(movie => {
+    return ( 
+            <div> 
+              <h2>{movie.title}</h2>
+              <p>Run Time: {movie.time}</p>
+              <ul>
+                 {(movie.genres).map(genre=>{
+                  return <li>{genre}</li>
+                 })} 
+              </ul>
+            </div>
+    )
+  })
+
+  
+  return ( 
+    <div>
+      <h1>Movies Page</h1>
+      {movieComponents}
+    </div>
+  )
 }
 
 export default Movies;
